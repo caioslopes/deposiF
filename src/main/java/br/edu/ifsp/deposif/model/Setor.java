d27cfb4356b4abfd264f13d616a21f617a4cb7cb
gutsserrano@gmail <gutsserrano@gmail.com>
2025-05-07 20:42:54 -0300

    corrigindo relacoes

--- src/main/java/br/edu/ifsp/deposif/model/Setor.java
+++ src/main/java/br/edu/ifsp/deposif/model/Setor.java
@@ -1,19 +1,26 @@
 package br.edu.ifsp.deposif.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import lombok.ToString;
+
+import java.util.Set;
 
 @Data
 @Entity
 @Table(name = "setor")
+@ToString(exclude = "produtos")
 public class Setor {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "setor_id")
     private Long id;
 
     @NotBlank(message = "Informe a descrição.")
     private String descricao;
+
+    @OneToMany
+    @JoinColumn(name = "produto_id")
+    private Set<Produto> produtos;
 }
