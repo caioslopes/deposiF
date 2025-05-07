941ee70628702b91db8c7c49a1f13a017c15798d
caioslopes <caiolopes.social@gmail.com>
2025-05-07 19:35:01 -0300

    initial commit

---
+++ src/main/groovy/br/edu/ifsp/deposif/DeposIfApplication.groovy
@@ -0,0 +1,13 @@
+package br.edu.ifsp.deposif
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DeposIfApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(DeposIfApplication, args)
+    }
+
+}
