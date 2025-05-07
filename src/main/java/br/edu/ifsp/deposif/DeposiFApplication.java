a13d64820c548039851aae7a10ff523f3d63b2e5
caioslopes <caiolopes.social@gmail.com>
2025-05-07 19:45:10 -0300

    initial commit

---
+++ src/main/java/br/edu/ifsp/deposif/DeposiFApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.deposif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeposiFApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeposiFApplication.class, args);
+    }
+
+}
