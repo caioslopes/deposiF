14586686ad8d38059b67be75f8738a9cfba63934
gutsserrano@gmail <gutsserrano@gmail.com>
2025-05-07 22:19:49 -0300

    nomes integrantes

--- src/main/java/br/edu/ifsp/deposif/DeposiFApplication.java
+++ src/main/java/br/edu/ifsp/deposif/DeposiFApplication.java
@@ -6,6 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DeposiFApplication {
 
+    /*
+        INTEGRANTES:
+        * Augusto G. Serrano
+        * Caio S. Lopes
+        * Igor F. Cardoso
+     */
+
     public static void main(String[] args) {
         SpringApplication.run(DeposiFApplication.class, args);
     }
